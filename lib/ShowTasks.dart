2af8eed21f2a7e23a6e91861a63499ec99d4632a
Wojciech Mankowski <wojtekm510@gmail.com>
2022-11-01 18:49:28 +0100

    connect the database

--- lib/ShowTasks.dart
+++ lib/ShowTasks.dart
@@ -1,35 +1,33 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:todolist/classes/Tasks.dart';
-import 'package:todolist/classes/Task.dart';
-
+import 'package:flutter/cupertino.dart';
 import 'Task.dart';
+import 'classes/Task.dart';
+import 'classes/Tasks.dart';
 
 class ShowTasks extends StatefulWidget {
-  const ShowTasks({Key? key, required this.tasks}) : super(key: key);
+  ShowTasks({Key? key, required this.tasks, required this.docIds})
+      : super(key: key);
   final Tasks tasks;
+  List<String> docIds = [];
   @override
-  State<ShowTasks> createState() => _ShowTasksState(tasks.getTasks());
+  State<ShowTasks> createState() => _ShowTasksState(tasks.getTasks(), docIds);
 }
 
 class _ShowTasksState extends State<ShowTasks> {
-  _ShowTasksState(this.tasks) {}
+  _ShowTasksState(this.tasks, this.docIds) {}
   List<Task> tasks;
+  List<String> docIds = [];
+  List items = [];
+  
+
   @override
   Widget build(BuildContext context) {
-    // for (int i = 0; i < this.tasks.length;) {
-    //   var task = this.tasks[i];
-    //   print("ZADANIE");
-    //   print(task.categories);
-    // }
     return Column(
       children: [
         for (int i = 0; i < this.tasks.length; i++)
           TaskWidget(
             task: this.tasks[i],
-          )
+          ),
       ],
     );
   }
 }
-
