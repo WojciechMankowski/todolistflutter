201bc3c1afdb1becf3f41af595520e10caf0f993
Wojciech Mankowski <52933584+WojciechMankowski@users.noreply.github.com>
2022-11-02 14:34:17 +0100

    Delete

--- lib/database.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_database/firebase_database.dart';
-
-// final DatabaseReference = FirebaseDatabase.instance.reference();
-final docTaks = FirebaseFirestore.instance.collection('tasks').snapshots();
-
-// final json = {'name': "Nauwka angielskiego", 'category': "Angielski"};
-// docTaks.Set() zapi danych do db
-//  zamianna klasy na jso nameclass.toJSon
